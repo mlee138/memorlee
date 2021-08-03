7751d2198f27756b6ad29f18f0c584cc500b33df
Matt <mlee138@binghamton.edu>
2021-08-03 12:06:22 -0400

    added radio buttons

--- src/components/Discover.js
+++ src/components/Discover.js
@@ -4,23 +4,47 @@ import {Image} from 'cloudinary-react'
 
 const Container = styled.div`
     padding: 2em 20%;
+    display: flex;
 `;
 
 const RadioContainer = styled.div`
-    display: grid;
-    grid-template-areas: 
-    'btn1 ... btn2'
-    'btn3 ... btn4';
-    grid-template-columns: 1fr 0.5fr 1fr;
+    display: flex;
+    flex-direction: column;
+    justify-content: space-around;
+    align-items: center;
+`;
+
+const Label = styled.label`
+    border: 3px solid blue;
+    border-radius: 5px;
+    padding: 0.5em 1em;
+    box-sizing: border-box;
+`;
+
+const Radio = styled.input`
 `;
 
 function Discover(){
     return(
         <Container>
             <Image publicId="sample" />
-            <div>
+            <RadioContainer>
+                <Label>FILL1
+                    <Radio type="radio" value="FILL" name="question"/>
+                </Label>
 
-            </div>
+                <label>FILL2
+                    <input type="radio" value="FILL" name="question"/>
+                </label>
+                
+                <label>FILL3
+                    <input type="radio" value="FILL" name="question"/>
+                </label>
+
+                <label>FILL4
+                    <input type="radio" value="FILL" name="question"/>
+                </label>
+            </RadioContainer>
         </Container>
     );
 }
