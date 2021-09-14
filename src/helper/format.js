1870bef060df2131ce46a76331e42e4ca9a61b74
Matt <mlee138@binghamton.edu>
2021-09-13 23:55:02 -0400

    merged main with upload branch

---
+++ src/helper/format.js
@@ -0,0 +1,18 @@
+function camelize(str) {
+    return str.replace(/(?:^\w|[A-Z]|\b\w)/g, function(word, index) {
+      return index === 0 ? word.toLowerCase() : word.toUpperCase();
+    }).replace(/\s+/g, '');
+}
+
+export function formatTripName(yr, loc) {
+  return `${yr}_${camelize(loc)}`
+}
+
+export function toTitleCase (str) {
+  return str
+    .trim()
+    .toLowerCase()
+    .split(' ')
+    .map(word => word.charAt(0).toUpperCase() + word.slice(1))
+    .join(' ');
+}
